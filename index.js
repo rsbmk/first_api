aee6dc1a6a160ecec0f02e1fbbf9bf8b7a8e117e
Rberto S. Bocio Melo <bociomelors@gmail.com>
2021-06-07 21:15:11 -0500

    My first API

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const cors = require('cors')
+
+const logger = require('./loggerMiddleware')
+let notes = require('./notes')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(logger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: 'note. content is missing'
+    })
+  }
+
+  const ids = notes.map(nota => nota.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+
+  }
+  notes = [...notes, newNote]
+
+  response.status(201).json(newNote)
+})
+
+// 404
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+// console.log(`Server runing on port ${PORT}`);
+})
---
+++ notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    id: 1,
+    content: 'hola como estas',
+    date: '2019/03/14',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Yo estoy super bien',
+    date: '2019/03/14',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'tengo que ir a pelarme',
+    date: '2019/03/14',
+    important: true
+  }
+]
+
+module.exports = notes
